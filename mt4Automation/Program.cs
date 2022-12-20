e0d0312602330dd1903dd1046072fc2e871a09bd
Maninder Kaur <maninder.kaur@serverlane.tech>
2022-12-20 16:29:57 +0530

    partial changes

--- mt4Automation/Program.cs
+++ mt4Automation/Program.cs
@@ -3,73 +3,106 @@ using System.Diagnostics;
 using FlaUI.Core;
 using FlaUI.Core.AutomationElements;
 using FlaUI.Core.Conditions;
+using FlaUI.Core.WindowsAPI;
 using FlaUI.UIA3;
 
 namespace Mt4Automation
 {
     class Program
-    {
+    { 
         static void Main(string[] args)
         {
-            int maxRetryCount = 20;
+            int maxRetryCount = int.MaxValue;
             bool exceptionRaised = false;
+            String[] liquidityProviders = {
+                        //"8001130: Dharmesh  Vinodray Kanabar",
+                        "56487: Vardhak Ashok Kamdar",
+                        "2030125: Dharmesh Vinodray Kanabar",
+                        "675233: Viplv Vijay Kumar",
+                        "14628787: Viplv Vijaykumar Jain",
+                        "2606012: Vardhak Ashok Kamdar",
+                        "372798: Honor Investments LLC",
+                        "1655839: Honor Investments LLC"
+                    };
             while (maxRetryCount > 0)
             {
-                String[] liquidityProviders = {
-                    "2030125: Dharmesh Vinodray Kanabar",
-                    "8001130: Dharmesh  Vinodray Kanabar",
-                    "56487: Vardhak Ashok Kamdar",
-                    "675233: Viplv Vijay Kumar",
-                    "14628787: Viplv Vijaykumar Jain",
-                    "2606012: Vardhak Ashok Kamdar",
-                    "372798: Honor Investments LLC",
-                    "1655839: Honor Investments LLC"
-                };
-                exceptionRaised = false;
-                
-                while (true)
-                {
-                    int i = 0;
-                    if (exceptionRaised) break;
+                //Initialize application
+                Application? application = Application.Launch("C:\\Program Files (x86)\\MetaTrader ECN - FXOpen\\terminal.exe");
+                using UIA3Automation automation = new UIA3Automation();
+                Window mainWindow = application.GetMainWindow(automation);
 
-                    //Initialize application
-                    var application = Application.Launch("C:\\Program Files (x86)\\MetaTrader ECN - FXOpen\\terminal.exe");
-                    var mainWindow = application.GetMainWindow(new UIA3Automation());
-                    ConditionFactory cf = new ConditionFactory(new UIA3PropertyLibrary());
-                    
-                    try
+                AutomationElement root = automation.GetDesktop();
+                ConditionFactory cf = new ConditionFactory(new UIA3PropertyLibrary());
+                AutomationElement loginWindow = root.FindFirstDescendant(cf.ByClassName("MetaQuotes::MetaTrader::4.00"));
+
+                string checkEaAlertProcessPath = "D:\\serverlane\\autoit_mt4_automation\\checkEaAlert.exe";
+                string eaConfirmationProcessPath = "D:\\serverlane\\autoit_mt4_automation\\eaConfirmation.exe";
+                exceptionRaised = false;
+
+                try
+                {
+                    for (int i = 0; i < liquidityProviders.Length; i++)
                     {
-                        while (i < liquidityProviders.Length)
+                        if (exceptionRaised) break;
+
+                        string lpTrying = new String(liquidityProviders[i].Where(Char.IsDigit).ToArray());
+                        string loggedLp = new String(loginWindow.Name.Where(Char.IsDigit).ToArray());
+
+                        if (loggedLp.Contains(lpTrying))
                         {
-                            Thread.Sleep(10000);
-                            mainWindow.FindFirstDescendant(cf.ByName("OK")).AsButton().Click();
-                            Thread.Sleep(1000);
-                            mainWindow.FindFirstDescendant(cf.ByName(liquidityProviders[i])).AsButton().DoubleClick();
-                            Thread.Sleep(2000);
-                            mainWindow.FindFirstDescendant(cf.ByName("Login")).AsButton().Click();
-                            Thread.Sleep(8000);
-                            mainWindow.FindFirstDescendant(cf.ByName("OK")).AsButton().Click();
-                            mainWindow.FindFirstDescendant(cf.ByName("Pytrader_MT4_EA_V2.07")).AsButton().DoubleClick();
-                            Thread.Sleep(2000);
-                            Process.Start("C:\\Users\\Administrator\\code\\autoit_mt4_automation\\checkEaAlert.exe");
-                            Thread.Sleep(2000);
-                            Process.Start("C:\\Users\\Administrator\\code\\autoit_mt4_automation\\eaConfirmation.exe");
-                            i++;
+                            continue;
                         }
-                        application.Close();
+
+                        Thread.Sleep(10000);
+                        mainWindow.FindFirstDescendant(cf.ByName("OK")).AsButton().Click();
+                        Thread.Sleep(1000);
+                        mainWindow.FindFirstDescendant(cf.ByName(liquidityProviders[i])).AsButton().DoubleClick();
+                        Thread.Sleep(2000);
+                        mainWindow.FindFirstDescendant(cf.ByName("Login")).AsButton().Click();
+                        Thread.Sleep(8000);
+                        mainWindow.FindFirstDescendant(cf.ByName("OK")).AsButton().Click();
+                        mainWindow.FindFirstDescendant(cf.ByName("Pytrader_MT4_EA_V2.07")).AsButton().DoubleClick();
+                        Thread.Sleep(2000);
+                        Process.Start(checkEaAlertProcessPath);
+                        Thread.Sleep(2000);
+                        Process.Start(eaConfirmationProcessPath);
                     }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine(e.ToString());
-                        exceptionRaised = true;
-                        application.Close();
-                        maxRetryCount--;
-                        break;
-                    }
+
+                    Program? program = new();
+                    program.KillProcess(checkEaAlertProcessPath);
+                    program.KillProcess(eaConfirmationProcessPath);
+                    program = null;
+                    application?.Kill();
+                    application?.Dispose();
                 }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                    application?.Kill();
+                    application?.Dispose();
+                    exceptionRaised = true;
+                    maxRetryCount--;
+                }
+                finally
+                {
+                    Thread.Sleep(180 * 1000); //3 minutes wait for UI automation memory leak
+                    GC.Collect();
+                    GC.WaitForPendingFinalizers();
+                    GC.Collect();
+
+                }
+
             }
 
-
         }
+        
+        public void KillProcess(string processName)
+        {
+            foreach (var process in Process.GetProcessesByName(processName))
+            {
+                process.Kill();
+            }
+        }
+    
     }
 }
